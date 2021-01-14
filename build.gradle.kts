d8d2052bf0a6841e1d2136565c4c0a4f2fde7f67
jntakpe <jntakpe@gmail.com>
2021-01-14 19:59:58 +0100

    initialize gradle kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "com.github.jntakpe"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
