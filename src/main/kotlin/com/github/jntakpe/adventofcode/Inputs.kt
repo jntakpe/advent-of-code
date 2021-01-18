9a2fe9aa991095c8ada090c4c22784d50c556733
jntakpe <jntakpe@gmail.com>
2021-01-18 23:00:55 +0100

    add readInput function that returns raw input string

--- src/main/kotlin/com/github/jntakpe/adventofcode/Inputs.kt
+++ src/main/kotlin/com/github/jntakpe/adventofcode/Inputs.kt
@@ -1,3 +1,5 @@
 package com.github.jntakpe.adventofcode
 
-fun readInputLines(puzzleNumber: Int) = object {}::class.java.getResource("/p$puzzleNumber.txt").readText().trim().lines()
+fun readInputLines(puzzleNumber: Int) = readInput(puzzleNumber).lines()
+
+fun readInput(puzzleNumber: Int) = object {}::class.java.getResource("/p$puzzleNumber.txt").readText().trim()
